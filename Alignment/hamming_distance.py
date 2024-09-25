be2ef8ca87121474009a46467d38d8ae6c3e0ddc
mayurathan <msrichandra@gmail.com>
2024-09-25 11:42:29 -0400

    Solved Counting point mutations, Hamming distance

---
+++ Alignment/hamming_distance.py
@@ -0,0 +1,20 @@
+# Hamming Distance
+
+#Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+#Return: The Hamming distance dH(s,t).
+
+def hammingDistance(dna1, dna2):
+    if len(dna1) != len(dna2):
+        print("Length of two dna sequences must match")
+
+    return sum(1 for i in range(len(dna1)) if dna1[i] != dna2[i])
+
+dna_file = "data/hamming_distance.txt"
+
+sequences = []
+with open(dna_file, 'r') as f:
+    for line in f:
+        line = line.strip()
+        sequences.append(line)
+
+print(hammingDistance(sequences[0], sequences[1]))
